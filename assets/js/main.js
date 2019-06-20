ba1b65dba4028abb6a2c2f5b35a04abece45cba7
Kira Corbin <madkatkeys@gmail.com>
2019-06-20 14:58:44 -0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const http = require("https://www.npmjs.com/package/node-spotify-api");
+var options = {
+    provider: "",
+    apiKey: "ncr9mGNOjQOdf2jKybbaoEWxgG3ZGxih"
+  };
+
+http.createServer(function(req, res){
+res.end("hello world")
+})
